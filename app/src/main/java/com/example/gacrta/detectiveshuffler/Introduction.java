319f78a9e4dbfb4c6e89b30a4fb07292994ecff3
Gabriel Almeida <gabrielcrta@gmail.com>
2017-01-23 15:12:40 -0200

    feat: initial commit

---
+++ app/src/main/java/com/example/gacrta/detectiveshuffler/Introduction.java
@@ -0,0 +1,23 @@
+package com.example.gacrta.detectiveshuffler;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Introduction extends Fragment {
+
+    public Introduction() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_introduction, container, false);
+    }
+
+}
